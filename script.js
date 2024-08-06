5bd079a283d56493b76f7cc0244c9cba0b2e3751
itzzJojo <jolenem3@nycstudents.net>
2024-08-06 16:04:43 +0000

    Built an age verification app for my home page

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello world")
+//console.log("hello world")
+
+//var prompt = prompt('What is your name?');
+
+//console.log(prompt);
+
+
+
+function verifyAge () {
+    var age = prompt("What is your age?")
+    console.log(age)
+
+    if (age < 18) { 
+        alert( 'You are a minor!')
+    } else if (age >=18 && age <= 60) { 
+        alert('You are an adult!') 
+    } else { 
+      alert ('You are a senior citizen!')
+  }
+}
